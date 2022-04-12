02671f1373592c3a1d765a4bb6c98ff8f5fc9360
Andrii <dzura1996das@gmail.com>
2022-04-12 15:38:54 +0300

    added hobbies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  AsyncValidatorFn,
+  FormArray,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'form-front';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  isDisabled: boolean = true;
+  units!: any[];
+  frameworks: any[] = [
+    {
+      framework: "Angular",
+      versions: ["1.1.1", "1.2.1", "1.3.3"],
+    },
+    {
+      framework: "Vue",
+      versions: ["3.3.1", "5.2.1", "5.3.1"],
+    },
+    {
+      framework: "React",
+      versions: ["2.1.2", "5.2.1", "4.3.1"],
+    },
+  ];
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl("", Validators.required),
+      lastName: new FormControl("", Validators.required),
+      birth: new FormControl(Validators.required),
+      framework: new FormControl("", Validators.required),
+      frameworkVersion: new FormControl("", Validators.required),
+      email: new FormControl(
+        "",
+        [Validators.required, Validators.email],
+        this.forbiddenEmails.bind(this) as AsyncValidatorFn
+      ),
+      hobbies: new FormArray([], Validators.required),
+    });
+
+    this.form.get("framework")?.valueChanges.subscribe((item) => {
+      this.units = item.versions;
+    });
+  }
+
+  getControls() {
+    return(<FormArray>this.form.get('hobbies') as FormArray).controls
+  }
+
+  onAddHobby() {
+    const hobby = new FormGroup({
+      title: new FormControl("", Validators.required),
+      duration: new FormControl("", Validators.required),
+    });
+    (<FormArray>this.form.get('hobbies')).push(hobby)
+  }
+
+  onDeleteHobby(hobbyIndex: number) {
+    
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "test@test.test") {
+          resolve({ emailForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.warn(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    FormsModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
